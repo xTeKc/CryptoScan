de9dd53aaee23c721a73ece66268016d3f6cfdf2
xTeKc <bcd3v@pm.me>
2021-08-02 21:54:40 -0400

    refactor

--- src/blockchains/binance.rs
+++ src/blockchains/binance.rs
@@ -5,6 +5,18 @@ extern crate colored;
 
 use http::{Request};
 
-fn main() {
-    
+fn binance_req() {
+    match reqwest::get("https://api.coingecko.com/api/v3/ping")
+        Ok(mut response) => {
+            //Check if 200 ok
+            if response.status() == reqwest::StatusCode::OK {
+                match response.text() {
+                    Ok(text) => println!("Response Text: {}", text),
+                    Err(_) => println!("Could not read response text")
+                }
+            } else {
+                println!("Response was not 200 Ok")
+            }
+        }
+        Err(_) => println!("Could not make request!")
 }
