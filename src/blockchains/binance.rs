6b84e68003610c6134976b6161a5d9dcadd99f6c
xTeKc <bcd3v@pm.me>
2021-08-04 23:51:16 -0400

    testing import

--- src/blockchains/binance.rs
+++ src/blockchains/binance.rs
@@ -1,16 +1,4 @@
 
 pub fn binance_req() {
-    match reqwest::get("https://api.coingecko.com/api/v3/ping")
-        Ok(mut response) => {
-            //Check if 200 ok
-            if response.status() == reqwest::StatusCode::OK {
-                match response.text() {
-                    Ok(text) => println!("Response Text: {}", text),
-                    Err(_) => println!("Could not read response text")
-                }
-            } else {
-                println!("Response was not 200 Ok")
-            }
-        }
-        Err(_) => println!("Could not make request!")
+    println!("Hello, From Binance.")
 }
