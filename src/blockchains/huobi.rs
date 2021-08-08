edce14983639a02051480533d5e3b116141e8c34
xTeKc <bcd3v@pm.me>
2021-08-07 22:30:18 -0400

    http req ok

--- src/blockchains/huobi.rs
+++ src/blockchains/huobi.rs
@@ -1,5 +1,15 @@
+use ureq;
 
+pub fn huobi_req() -> Result<(), ureq::Error> {
+  let req: String = ureq::get("https://api.coingecko.com/api/v3/ping")
+  .set("Example-Header", "header value")
+  .call()?
+  .into_string()?;
+  println!("{:?}", req);
+Ok(())
+}
 
-pub fn huobi_req() {
-    println!("Hello, From Huobi.");
+pub fn call_huobi_req() {
+  let b = huobi_req();
+  println!("{:?}", b);
 }
