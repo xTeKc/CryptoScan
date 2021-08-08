cb574d1acf9a72710b2f1ace5735c79e0c2a1eaf
xTeKc <bcd3v@pm.me>
2021-08-07 22:31:55 -0400

    http req ok

--- src/blockchains/xdai.rs
+++ src/blockchains/xdai.rs
@@ -1,5 +1,15 @@
+use ureq;
 
+pub fn xdai_req() -> Result<(), ureq::Error> {
+  let req: String = ureq::get("https://api.coingecko.com/api/v3/ping")
+  .set("Example-Header", "header value")
+  .call()?
+  .into_string()?;
+  println!("{:?}", req);
+Ok(())
+}
 
-pub fn xdai_req() {
-    println!("Hello, From xDai.");
+pub fn call_xdai_req() {
+  let b = xdai_req();
+  println!("{:?}", b);
 }
