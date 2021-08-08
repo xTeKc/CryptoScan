6fb427f63bde234e983d7d526999dee09831df67
xTeKc <bcd3v@pm.me>
2021-08-07 22:31:25 -0400

    http req ok

--- src/blockchains/solana.rs
+++ src/blockchains/solana.rs
@@ -1,5 +1,15 @@
+use ureq;
 
+pub fn solana_req() -> Result<(), ureq::Error> {
+  let req: String = ureq::get("https://api.coingecko.com/api/v3/ping")
+  .set("Example-Header", "header value")
+  .call()?
+  .into_string()?;
+  println!("{:?}", req);
+Ok(())
+}
 
-pub fn solana_req() {
-    println!("Hello, From Solana.");
+pub fn call_solana_req() {
+  let b = solana_req();
+  println!("{:?}", b);
 }
