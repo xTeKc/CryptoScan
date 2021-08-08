85228ac49e5a717391ff403e0bf75a247fb2fb52
xTeKc <bcd3v@pm.me>
2021-08-07 22:30:52 -0400

    http req ok

--- src/blockchains/polygon.rs
+++ src/blockchains/polygon.rs
@@ -1,5 +1,15 @@
+use ureq;
 
+pub fn polygon_req() -> Result<(), ureq::Error> {
+  let req: String = ureq::get("https://api.coingecko.com/api/v3/ping")
+  .set("Example-Header", "header value")
+  .call()?
+  .into_string()?;
+  println!("{:?}", req);
+Ok(())
+}
 
-pub fn polygon_req() {
-    println!("Hello, From Polygon.");
+pub fn call_polygon_req() {
+  let b = polygon_req();
+  println!("{:?}", b);
 }
