a97b329454c280a94b49be5bda364af29162aba8
xTeKc <bcd3v@pm.me>
2021-08-07 22:29:01 -0400

    http req ok

--- src/blockchains/fantom.rs
+++ src/blockchains/fantom.rs
@@ -1,5 +1,15 @@
+use ureq;
 
+pub fn fantom_req() -> Result<(), ureq::Error> {
+  let req: String = ureq::get("https://api.coingecko.com/api/v3/ping")
+  .set("Example-Header", "header value")
+  .call()?
+  .into_string()?;
+  println!("{:?}", req);
+Ok(())
+}
 
-pub fn fantom_req() {
-    println!("Hello, From Fantom.");
+pub fn call_fantom_req() {
+  let b = fantom_req();
+  println!("{:?}", b);
 }
