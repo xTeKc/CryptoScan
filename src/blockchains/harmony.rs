526f05fe6ef575035af83ca066f27f70d99aa391
xTeKc <bcd3v@pm.me>
2021-08-07 22:29:43 -0400

    http req ok

--- src/blockchains/harmony.rs
+++ src/blockchains/harmony.rs
@@ -1,5 +1,15 @@
+use ureq;
 
+pub fn harmony_req() -> Result<(), ureq::Error> {
+  let req: String = ureq::get("https://api.coingecko.com/api/v3/ping")
+  .set("Example-Header", "header value")
+  .call()?
+  .into_string()?;
+  println!("{:?}", req);
+Ok(())
+}
 
-pub fn harmony_req() {
-    println!("Hello, From Harmony.");
+pub fn call_harmony_req() {
+  let b = harmony_req();
+  println!("{:?}", b);
 }
