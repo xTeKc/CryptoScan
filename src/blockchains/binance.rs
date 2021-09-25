d60dbdc29535483d0670329af8236629c813cbb1
xTeKc <bcd3v@pm.me>
2021-09-24 22:54:56 -0400

    revert to orgin

--- src/blockchains/binance.rs
+++ src/blockchains/binance.rs
@@ -1,16 +1,13 @@
 use ureq::*;
 
-// pub fn binance_req() -> Result<(), ureq::Error> {
-//   let req: String = ureq::get("https://api.coingecko.com/api/v3/ping")
-//   .set("Example-Header", "header value")
-//   .call()?
-//   .into_string()?;
-//   println!("{:?}", req);
-// Ok(())
-// }
-
-
-
+pub fn binance_req() -> Result<(), ureq::Error> {
+  let req: String = ureq::get("https://api.coingecko.com/api/v3/ping")
+  .set("Example-Header", "header value")
+  .call()?
+  .into_string()?;
+  println!("{:?}", req);
+Ok(())
+}
 
 pub fn call_binance_req() {
   let b = binance_req();
