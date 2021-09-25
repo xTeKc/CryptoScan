a7f35aa300e8eca7f810ef5fe846e6ffe3d408f0
xTeKc <bcd3v@pm.me>
2021-09-24 21:23:23 -0400

    comment out

--- src/blockchains/binance.rs
+++ src/blockchains/binance.rs
@@ -1,13 +1,18 @@
 use ureq;
 
-pub fn binance_req() -> Result<(), ureq::Error> {
-  let req: String = ureq::get("https://api.coingecko.com/api/v3/ping")
-  .set("Example-Header", "header value")
-  .call()?
-  .into_string()?;
-  println!("{:?}", req);
-Ok(())
-}
+// pub fn binance_req() -> Result<(), ureq::Error> {
+//   let req: String = ureq::get("https://api.coingecko.com/api/v3/ping")
+//   .set("Example-Header", "header value")
+//   .call()?
+//   .into_string()?;
+//   println!("{:?}", req);
+// Ok(())
+// }
+
+// pub fn binance_req() {
+//   let req = ureq::get("https://api.coingecko.com/api/v3/ping");
+//   println!("{:?}", req);
+// }
 
 pub fn call_binance_req() {
   let b = binance_req();
