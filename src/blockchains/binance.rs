cec033f139e9efcd5eda184d8a92f14d46681d75
xTeKc <bcd3v@pm.me>
2021-09-24 22:35:39 -0400

    remove exaggerated http depend and add new http depend

--- src/blockchains/binance.rs
+++ src/blockchains/binance.rs
@@ -1,4 +1,4 @@
-
+use ureq::*;
 
 // pub fn binance_req() -> Result<(), ureq::Error> {
 //   let req: String = ureq::get("https://api.coingecko.com/api/v3/ping")
@@ -9,14 +9,8 @@
 // Ok(())
 // }
 
-#[tokio::main]
-pub async fn binance_req() {
-  let req = reqwest::get("https://api.coingecko.com/api/v3/ping")
-    .await?
-    .text()
-    .await?
-  println!("{:?}", req);
-}
+
+
 
 pub fn call_binance_req() {
   let b = binance_req();
