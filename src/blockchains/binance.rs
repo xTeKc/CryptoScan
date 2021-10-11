f2adc23a1dbd2fd2455b958575c468ce3fe68e71
xTeKc <bcd3v@pm.me>
2021-10-11 11:44:17 -0400

    restart func

--- src/blockchains/binance.rs
+++ src/blockchains/binance.rs
@@ -9,14 +9,18 @@ pub struct Crypto {
   // add data
 }
 
-pub fn binance_req() -> Result<(), ureq::Error> {
-  let req: String = ureq::get("https://api.coingecko.com/api/v3/global")
-  .set("Example-Header", "header value")
-  .call()?
-  .into_string()?;
-  println!("{:?}", req);
-Ok(())
+pub fn binance_req() {
+  
 }
+
+// pub fn binance_req() -> Result<(), ureq::Error> {
+//   let req: String = ureq::get("https://api.coingecko.com/api/v3/global")
+//   .set("Example-Header", "header value")
+//   .call()?
+//   .into_string()?;
+//   println!("{:?}", req);
+// Ok(())
+// }
 
 pub fn call_binance_req() {
   let b = binance_req();
