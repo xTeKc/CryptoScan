e1f7d8fc6e873488635e37e3034c9334d484e0d9
xTeKc <bcd3v@pm.me>
2021-10-12 15:12:12 -0400

    remove request func

--- src/blockchains/cardano.rs
+++ src/blockchains/cardano.rs
@@ -1,13 +1,6 @@
 use ureq;
 
-pub fn cardano_req() -> Result<(), ureq::Error> {
-  let req: String = ureq::get("https://api.coingecko.com/api/v3/ping")
-  .set("Example-Header", "header value")
-  .call()?
-  .into_string()?;
-  println!("{:?}", req);
-Ok(())
-}
+
 
 pub fn call_cardano_req() {
   let b = cardano_req();
