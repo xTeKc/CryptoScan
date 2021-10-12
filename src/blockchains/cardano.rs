d5c584f481e6f67f6f56d87fdf233bb9a3356310
xTeKc <bcd3v@pm.me>
2021-10-12 15:19:13 -0400

    add debug module

--- src/blockchains/cardano.rs
+++ src/blockchains/cardano.rs
@@ -18,7 +18,9 @@ pub fn cardano_req(req: &str) -> Result<Cryptos, Box<dyn Error>> {
   let resp = ureq::get(req).call()?.into_string()?;
 
   let cryptos: Cryptos = serde_json::from_str(&resp)?;
-  
+
+  dbg!(cryptos);
+
 }
 
 pub fn call_cardano_req() {
