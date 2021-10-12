545a554568c5d4e1aae250a050fec243471319a2
xTeKc <bcd3v@pm.me>
2021-10-12 15:21:25 -0400

    add cryptos var

--- src/blockchains/cardano.rs
+++ src/blockchains/cardano.rs
@@ -26,5 +26,6 @@ pub fn cardano_req(req: &str) -> Result<Cryptos, Box<dyn Error>> {
 
 pub fn call_cardano_req() {
   let req = "https://api.coingecko.com/api/v3/global";
+  let cryptos = cardano_req(req);
   println!("{:?}", b);
 }
