fe963a2d2978eb117cc776db5c33db4956f79e5e
xTeKc <bcd3v@pm.me>
2021-10-13 15:54:40 -0400

    clear request func

--- src/blockchains/ethereum.rs
+++ src/blockchains/ethereum.rs
@@ -12,13 +12,8 @@ pub struct Crypto {
   marketcap: u32
 }
 
-pub fn ethereum_req() -> Result<(), ureq::Error> {
-  let req: String = ureq::get("https://api.coingecko.com/api/v3/ping")
-  .set("Example-Header", "header value")
-  .call()?
-  .into_string()?;
-  println!("{:?}", req);
-Ok(())
+pub fn ethereum_req() {
+
 }
 
 pub fn call_ethereum_req() {
