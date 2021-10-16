d50a96e1044118d5e4ec32d2458539f579be3242
xTeKc <bcd3v@pm.me>
2021-10-15 23:34:39 -0400

    refactor

--- src/blockchains/fantom.rs
+++ src/blockchains/fantom.rs
@@ -24,7 +24,6 @@ pub fn fantom_req(req: &str) -> Result<Cryptos, Box<dyn Error>> {
   todo!()
 }
 
-
 pub fn call_fantom_req() {
   let req = "https://api.coingecko.com/api/v3/global";
   let cryptos = fantom_req(req);
