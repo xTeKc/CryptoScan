bb3e9f98e48200424fe68a348b2d104ee646b168
xTeKc <bcd3v@pm.me>
2021-10-15 23:29:21 -0400

    clear request func

--- src/blockchains/fantom.rs
+++ src/blockchains/fantom.rs
@@ -12,14 +12,10 @@ pub struct Crypto {
   marketcap: u32
 }
 
-pub fn fantom_req() -> Result<(), ureq::Error> {
-  let req: String = ureq::get("https://api.coingecko.com/api/v3/ping")
-  .set("Example-Header", "header value")
-  .call()?
-  .into_string()?;
-  println!("{:?}", req);
-Ok(())
+pub fn fantom_req() {
+  
 }
+
 
 pub fn call_fantom_req() {
   let b = fantom_req();
