7a2717f403b264842487ff7a1be740ac6620623b
xTeKc <bcd3v@pm.me>
2021-10-15 23:33:48 -0400

    add crypto var to call request

--- src/blockchains/fantom.rs
+++ src/blockchains/fantom.rs
@@ -27,5 +27,5 @@ pub fn fantom_req(req: &str) -> Result<Cryptos, Box<dyn Error>> {
 
 pub fn call_fantom_req() {
   let req = "https://api.coingecko.com/api/v3/global";
-
+  let cryptos = fantom_req(req);
 }
