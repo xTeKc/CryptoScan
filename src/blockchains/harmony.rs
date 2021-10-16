3dab956368b1af38749b0aef01458bf6118f5b61
xTeKc <bcd3v@pm.me>
2021-10-16 08:35:27 -0400

    clear request func

--- src/blockchains/harmony.rs
+++ src/blockchains/harmony.rs
@@ -2,13 +2,8 @@
 use std::error::Error;
 use serde::Deserialize;
 
-pub fn harmony_req() -> Result<(), ureq::Error> {
-  let req: String = ureq::get("https://api.coingecko.com/api/v3/ping")
-  .set("Example-Header", "header value")
-  .call()?
-  .into_string()?;
-  println!("{:?}", req);
-Ok(())
+pub fn harmony_req(){
+  
 }
 
 pub fn call_harmony_req() {
