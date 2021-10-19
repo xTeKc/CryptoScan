1a2b45a18cb2a93ab60d878266c3ca8461a92a8d
xTeKc <bcd3v@pm.me>
2021-10-19 16:11:55 -0400

    derive debug

--- src/blockchains/solana.rs
+++ src/blockchains/solana.rs
@@ -2,10 +2,12 @@
 use std::error::Error;
 use serde::Deserialize;
 
+#[derive(Deserialize, Debug)]
 pub struct Cryptos {
   cryptos: Vec<Crypto>
 }
 
+#[derive(Deserialize, Debug)]
 pub struct Crypto {
   // name: String,
   symbol: String,
@@ -18,7 +20,7 @@ pub fn solana_req(req: &str) -> Result<Cryptos, Box<dyn Error>> {
   let cryptos: Cryptos = serde_json::from_str(&resp)?;
 
   dbg!(cryptos);
-  
+
   todo!()
 }
 
