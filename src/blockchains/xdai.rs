84a78068b37bb465d8b8aa0a3ec75c20ecfb0028
xTeKc <bcd3v@pm.me>
2021-10-21 15:15:56 -0400

    update imports

--- src/blockchains/xdai.rs
+++ src/blockchains/xdai.rs
@@ -1,4 +1,6 @@
-use ureq;
+// use ureq::*;
+use std::error::Error;
+use serde::Deserialize;
 
 pub fn xdai_req() -> Result<(), ureq::Error> {
   let req: String = ureq::get("https://api.coingecko.com/api/v3/ping")
