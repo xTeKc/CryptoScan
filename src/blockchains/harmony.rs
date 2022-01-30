5905cf0ddf67624335d10b6a8f357c60747f8ac8
xTeKc <bcd3v@pm.me>
2022-01-30 00:21:22 -0500

    add _

--- src/blockchains/harmony.rs
+++ src/blockchains/harmony.rs
@@ -34,7 +34,7 @@ pub async fn harmony_req(req: &str) -> Result<Cryptos, Box<dyn Error>> {
 
 pub fn call_harmony_req() {
   let req = "https://api.coingecko.com/api/v3/global";
-  let cryptos = harmony_req(req);
-  dbg!(cryptos);
+  let _cryptos = harmony_req(req);
+  dbg!(_cryptos);
   println!("{req}");
 }
