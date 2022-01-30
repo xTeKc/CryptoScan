1469a59764b09979edab6c1111b6fb5731a5f3e1
xTeKc <bcd3v@pm.me>
2022-01-30 00:20:44 -0500

    add _

--- src/blockchains/cardano.rs
+++ src/blockchains/cardano.rs
@@ -34,7 +34,7 @@ pub async fn cardano_req(req: &str) -> Result<Cryptos, Box<dyn Error>> {
 
 pub fn call_cardano_req() {
   let req = "https://api.coingecko.com/api/v3/global";
-  let cryptos = cardano_req(req);
-  dbg!(cryptos);
+  let _cryptos = cardano_req(req);
+  dbg!(_cryptos);
   println!("{req}");
 }
