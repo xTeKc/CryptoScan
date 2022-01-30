eae805ba2ffe9d4ac7099a4d0ed75b038458fe7f
xTeKc <bcd3v@pm.me>
2022-01-30 00:20:57 -0500

    add _

--- src/blockchains/ethereum.rs
+++ src/blockchains/ethereum.rs
@@ -34,7 +34,7 @@ pub async fn ethereum_req(req: &str) -> Result<Cryptos, Box<dyn Error>> {
 
 pub fn call_ethereum_req() {
   let req = "https://api.coingecko.com/api/v3/global";
-  let cryptos = ethereum_req(req);
-  dbg!(cryptos);
+  let _cryptos = ethereum_req(req);
+  dbg!(_cryptos);
   println!("{req}");
 }
