a0c762a473ec246d8390d712eda9d3090abfab28
xTeKc <bcd3v@pm.me>
2022-01-30 00:21:58 -0500

    add _

--- src/blockchains/solana.rs
+++ src/blockchains/solana.rs
@@ -34,7 +34,7 @@ pub async fn solana_req(req: &str) -> Result<Cryptos, Box<dyn Error>> {
 
 pub fn call_solana_req() {
   let req = "https://api.coingecko.com/api/v3/global";
-  let cryptos = solana_req(req);
-  dbg!(cryptos);
+  let _cryptos = solana_req(req);
+  dbg!(_cryptos);
   println!("{req}");
 }
